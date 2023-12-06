dfc2ad0dad01ea68fce20d0e9210871f6558d6e9
KOVUCZ <Hrdy.Adam@stud.vassboskovice.cz>
2023-12-06 12:46:59 +0100

    už to funguje, kód i otestován

--- Hrdy_tretiPokus_skupA_prikladPrvni_20231606/MainWindow.xaml.cs
+++ Hrdy_tretiPokus_skupA_prikladPrvni_20231606/MainWindow.xaml.cs
@@ -32,15 +32,24 @@ namespace Hrdy_tretiPokus_skupA_prikladPrvni_20231606
             double v_kvadru;
             double objem;
 
+            try
+            {
+                a = double.Parse(inputA.Text);
+                b = double.Parse(inputB.Text);
+                c = double.Parse(inputC.Text);
+                v_kvadru = (a * b * c);
+                v_valce = Math.PI * a * c; //já debil tam dal poloměr a on je to průměr *facepalm*
+                objem = v_kvadru - v_valce;
+                Output.Content = $"Objem kvádru je: {v_kvadru} \r\n Objem válce je: {v_valce} \r\n Objem kvádru mínus válce: {objem}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"špatná hodnota, zadávej pouze čisla! - {ex.Message}");
+            }
 
+            
 
-            a = double.Parse(inputA.Text);
-            b = double.Parse(inputB.Text);
-            c = double.Parse(inputC.Text);
-            v_kvadru = (a * b * c);
-            v_valce = Math.PI * (a * a) * c;
-            objem = v_kvadru - v_valce;
-            Output.Content = $"Objem kvádru je {v_kvadru} \r\n Objem válce je {v_valce} \r\n Objem kvádru mínus válce {objem}";
+            
 
         }
     }
